0f81ede197e37ea8391bbe309943de9b9fbb3b45
arief <dvkentury@gmail.com>
2021-11-28 18:19:51 +0700

    create handler for lebih banyak button

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
-// HANDLE OPEN MENU 
+// HANDLE OPEN MENU
 
-document.querySelector('#open-menu').addEventListener('click', () => {
-    let buttonOpenMenuIcon = document.querySelector('#open-menu i');
-    let sideMenu = document.querySelector('nav ul');
+document.querySelector("#open-menu").addEventListener("click", () => {
+  let buttonOpenMenuIcon = document.querySelector("#open-menu i");
+  let sideMenu = document.querySelector("nav ul");
 
-    if(sideMenu.classList.contains('close')) {
-        sideMenu.classList.remove('close');
-        sideMenu.classList.add('open');
-        sideMenu.style.right = '0';
+  if (sideMenu.classList.contains("close")) {
+    sideMenu.classList.remove("close");
+    sideMenu.classList.add("open");
+    sideMenu.style.right = "0";
 
-        buttonOpenMenuIcon.classList.remove('fa-bars')
-        buttonOpenMenuIcon.classList.add('fa-times');
-      } else {
-        sideMenu.classList.add('close');
-        sideMenu.classList.remove('open');
-        sideMenu.style.right = '-31.25rem';
+    buttonOpenMenuIcon.classList.remove("fa-bars");
+    buttonOpenMenuIcon.classList.add("fa-times");
+  } else {
+    sideMenu.classList.add("close");
+    sideMenu.classList.remove("open");
+    sideMenu.style.right = "-31.25rem";
 
-        buttonOpenMenuIcon.classList.add('fa-bars')
-        buttonOpenMenuIcon.classList.remove('fa-times')
-      }
-})
+    buttonOpenMenuIcon.classList.add("fa-bars");
+    buttonOpenMenuIcon.classList.remove("fa-times");
+  }
+});
+
+// HANDLE LOAD MORE
+let loadMore = document.getElementById("load-more");
+let moreText = document.getElementById("more");
+
+loadMore.addEventListener("click", () => {
+  if (moreText.classList.contains('hide')) {
+    moreText.classList.remove('hide');
+    moreText.classList.add('show');
+    loadMore.innerText = "Kurangi"
+  } else {
+    moreText.classList.add('hide');
+    moreText.classList.remove('show');
+    loadMore.innerText = "Lebih Banyak";
+  }
+});
